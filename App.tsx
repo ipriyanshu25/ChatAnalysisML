0af259572f2a2af817bbc3f4ff7834c23972eb47
Priyanshu Yadav <114854534+ipriyanshu25@users.noreply.github.com>
2024-10-25 01:38:42 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,29 +1,3 @@
-// import "./App.css";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import LoginForm from "./login";
-// import Table from "./table";
-// import HomePage  from "./home";
-// import Counter from "./counter";
-// import Register from "./register";
-// import Exam from "./exam";
-
-
-// const App = () => {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/home" element={<HomePage test ="This is a sample home page."/>} />
-//         <Route path="/login" element={<LoginForm />} />
-//         <Route path="/counter" element={<Counter />} />
-//         <Route path="/table" element={<Table />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/exam" element={<Exam />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// };
-
-// export default App;
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ChatAnalysisDashboard from './chatanalysisdashboard';
